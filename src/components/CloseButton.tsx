65c0c9e23c22e982d5d9e0bf43ccbe8de49ce587
Charles Samuels <chasms@chas.ms>
2025-11-24 14:46:40 -0500

    Updated close button with same style opening and closing

--- src/components/CloseButton.tsx
+++ src/components/CloseButton.tsx
@@ -1,21 +1,13 @@
 interface Props {
-  onClick: () => void;
+  setIsOpen: (isOpen: boolean) => void;
+  isOpen: boolean;
 }
 
-const CloseButton = ({ onClick }: Props) => (
-  <div className="fixed bottom-1/3 translate-y-6 right-0 -ml-8 flex pt-4 pr-2 duration-500 ease-in-out group-data-closed/dialog-panel:opacity-0 sm:pr-4">
-    <button
-      type="button"
-      onClick={onClick}
-      className="relative rounded-md text-blue-400 hover:text-blue-200 focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600 cursor-pointer"
-    >
-      <span className="absolute -inset-2.5"></span>
-      <span className="sr-only">Close panel</span>
-      <svg viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1.5" data-slot="icon" aria-hidden="true" className="size-6">
-        <path d="M6 18 18 6M6 6l12 12" stroke-linecap="round" stroke-linejoin="round" />
-      </svg>
-    </button>
-  </div>
+const CloseButton = ({ isOpen, setIsOpen }: Props) => (
+  <div
+    className={`border-r-4 border-b-4 w-6 h-6 fixed right-1/2 translate-x-3 hover:w-7 hover:h-7 hover:translate-x-3.5 cursor-pointer transition-all duration-100 ease-in-out z-10 ${isOpen ? "bottom-1/3 -translate-y-2 rotate-45 border-blue-400 hover:border-blue-200" : "bottom-12 rotate-225 border-blue-900 hover:border-blue-400"}`}
+    onClick={() => setIsOpen(!isOpen)}
+  />
 );
 
 export default CloseButton;
--- src/components/CountriesList.tsx
+++ src/components/CountriesList.tsx
@@ -59,19 +59,12 @@ const CountriesList = () => {
           />
         ))}
       </div>
-      {isDrawerOpen || (
-        <div
-          className="border-r-4 border-b-4 border-blue-900 w-6 h-6 fixed right-1/2 bottom-12 rotate-225 translate-x-3 hover:border-blue-400 hover:w-7 hover:h-7 hover:translate-x-3.5 cursor-pointer transition-all duration-100 ease-in-out"
-          onClick={() => setIsDrawerOpen(true)}
-        />
-      )}
+      <CloseButton isOpen={isDrawerOpen} setIsOpen={setIsDrawerOpen} />
       <div
         id="drawer"
         aria-labelledby="drawer-title"
         className={`fixed bottom-0 w-full bg-slate-800 shadow-lg outline outline-black/5 transition-all duration-300 ease-in-out ${isDrawerOpen ? "h-1/3 overflow-auto" : "h-12 overflow-hidden"}`}
       >
-        {isDrawerOpen && <CloseButton onClick={() => setIsDrawerOpen(false)} />}
-
         <CountryComparison countries={bookmarkedCountries} setCountryBookmark={setCountryBookmark} />
       </div>
     </>
