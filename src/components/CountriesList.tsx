1eb6435d4cd0689fc54eea11fa3fea75f31d60f4
Charles Samuels <chasms@chas.ms>
2025-11-24 13:24:56 -0500

    Initial Drawer for bookmark comparison

--- src/components/CountriesList.tsx
+++ src/components/CountriesList.tsx
@@ -1,6 +1,7 @@
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import type { Country } from "../types";
 import CountryCard from "./CountryCard";
+import CountryComparison from "./CountryComparison";
 
 const localStorageCountryBookmarksKey = "CountryBookmarks";
 
@@ -43,17 +44,27 @@ const CountriesList = () => {
     getAndSetCountries();
   }, []);
 
+  const bookmarkedCountries = useMemo(() => countries.filter((country) => !!countryBookmarks[country.flag]), [countries, countryBookmarks]);
   return (
-    <div className="grid grid-cols-2 gap-4 p-4">
-      {countries?.map((country) => (
-        <CountryCard
-          key={country.name.official}
-          country={country}
-          setCountryBookmark={() => setCountryBookmark(country.flag)}
-          isBookmarked={!!countryBookmarks[country.flag]}
-        />
-      ))}
-    </div>
+    <>
+      <div className="grid grid-cols-2 gap-4 p-4">
+        {countries?.map((country) => (
+          <CountryCard
+            key={country.name.official}
+            country={country}
+            setCountryBookmark={() => setCountryBookmark(country.flag)}
+            isBookmarked={!!countryBookmarks[country.flag]}
+          />
+        ))}
+      </div>
+      <div
+        id="drawer"
+        aria-labelledby="drawer-title"
+        className="fixed bottom-0 w-full max-h-1/3 overflow-scroll max-w-none bg-slate-800 shadow-lg outline outline-black/5"
+      >
+        <CountryComparison countries={bookmarkedCountries} setCountryBookmark={setCountryBookmark} />
+      </div>
+    </>
   );
 };
 
---
+++ src/components/CountryComparison.tsx
@@ -0,0 +1,19 @@
+import type { Country } from "../types";
+import CountryCard from "./CountryCard";
+
+interface Props {
+  countries: Country[];
+  setCountryBookmark: (countryFlag: Country["flag"]) => void;
+}
+
+const CountryComparison = ({ countries, setCountryBookmark }: Props) => {
+  return (
+    <div className="grid grid-cols-2 gap-4 p-4">
+      {countries.map((country) => (
+        <CountryCard country={country} isBookmarked={true} setCountryBookmark={() => setCountryBookmark(country.flag)} />
+      ))}
+    </div>
+  );
+};
+
+export default CountryComparison;
